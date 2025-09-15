22c8f63b85921f693f52a7feabcd36e333d0e44d
adarshl02 <adarsh.landge10604@gmail.com>
2025-09-15 19:22:14 +0530

    1

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
+  public_key = file("D:/Desktop/Projects/mega-project/terraform/terra-key.pub")
 }
 
 resource "aws_default_vpc" "default" {
